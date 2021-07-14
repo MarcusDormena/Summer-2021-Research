6e1e44bace9f1a1cf1be8c2bb8240fe60c80bcf7
Marcus Dormena <mdormena@hamilton.internal.amnh.org>
2021-07-14 20:44:45 +0000

    Adding learnfunction code

---
+++ learnfunction.py
@@ -0,0 +1,14 @@
+#This code is where we learn to define a function
+#The word "def" defines a function
+#we  will make one that adds
+
+def addfunction(x,y):
+    result = x + y
+    return result
+
+# to use it:
+num1 = 5
+num2 = 3
+
+answer = addfunction(num1,num2)
+print(answer)
