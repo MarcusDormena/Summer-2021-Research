c98c244b11bf51fbc9a15018f7fa06f284a0375d
Marcus Dormena <mdormena@hamilton.internal.amnh.org>
2021-07-15 19:15:51 +0000

    Added h=s.halos() and typo

--- findBHnew.py
+++ findBHnew.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pynbody
 import matplotlib.pyplot as plt
-from pynbody import filt
+from pynbody import filt, array
 
 s=pynbody.load('/mnt/data0/jillian/h258/stampedetesting/glenna/take2/h258.cosmo50cmb.3072gst1bwdK1BH.000048')
-
+h=s.halos()
 s.physical_units()
 
 #function to find black holes
@@ -15,12 +15,9 @@ def findBH(s):
 BH = findBH(s)
 print("The number of Black Holes is: ",len(BH))
 
-BH = findBH(s)
-print(BH)
-
 #Function to find which halo (galaxy) the BH is in
 def findBHhalos(s):
-    BH = findBH(s)
     BHhalos = BH['amiga.grp']
     return BHhalos
+BHhalos = findBHhalos(s)
 print(BHhalos)
