12fe3b31c2858e38f40c89c2794988fbaac48bf7
Marcus Dormena <mdormena@hamilton.internal.amnh.org>
2021-07-21 21:20:00 +0000

    Fixed array issue

--- findBHnew.py
+++ findBHnew.py
@@ -7,7 +7,7 @@ import pandas as pd
 newArray=[21,3]
 s=pynbody.load('/mnt/data0/jillian/h258/stampedetesting/glenna/take2/h258.cosmo50cmb.3072gst1bwdK1BH.000048')
 h=s.halos()
-s.physical_units
+s.physical_units()
 
 #function to find black holes:
 def findBH(s):
@@ -21,38 +21,23 @@ print("The number of Black Holes is",len(BH))
 def findBHhalos(s):
     BHhalos = BH['amiga.grp']
     return BHhalos
+
 BHhalos = findBHhalos(s)
+print(BHhalos)
+for i in range(len(BH)):
+    if BHhalos[i] ==0:
+        continue
+    #print(BHhalos[i])
+    pynbody.analysis.halo.center(h[BHhalos[i]], mode='hyb')
+    x=BH['pos'][[i],0]
+    y=BH['pos'][[i],1]
+    z=BH['pos'][[i],2]
+    distance = (x**2+y**2+z**2)**0.5
+    data = BH['iord'][i], BHhalos[i], distance[0]
+    print(data)
 
-#To find how far BH's are from thier galaxy's center:
-pynbody.analysis.halo.center(h[4], mode='hyb'):
-    df = pd.DataFrame(data=BHhalos, columns=['Galaxy'])
-    print("These are the galaxies in which each Black Hole is in:" ,h[4]['pos'][0],h[4]['pos'][1], h[4]['pos'][2])
-
-    #print(h[4]['pos'][0])
-    #print(h[4]['pos'][1])
-    #print(h[4]['pos'][2])
-#Above, [0],[1], and [2] mean x, y, and z respectively.
-#position of BH:
-BHposition = BH['pos']
-print("Here are the Black Hole Positions:", BHposition)
-
-#To put x,y, and z values into columns:
-BHx1 = BHposition[[0],0]
-BHx2 = BHposition[[1],0]
-
-BHy1 = BHposition[[0],1]
-BHy2 = BHposition[[1],1]
-
-BHz1 = BHposition[[0],2]
-BHz2 = BHposition[[1],2]
-
-#Distance formula
-distance = ((BHx2-BHx1)**2+(BHy2-BHy1)**2+(BHz2-BHz1)**2)**0.5
-print("The distance between the Black Hole and the Galaxy center is ", distance, "kiloparsecs.")
-
-#Table with BH ID#, Host Galaxy, and Distance from Center
-#newArray = array with ID#, galaxy, and distance.
 # BH?, BHhalos, distance 
 #df2 = pd.DataFrame(data=newArray, columns=['Black Hole ID#','Host Galaxy','Distance (kpc)'])
 #print(df2)
-print(BH)
+#print(BH)
+
