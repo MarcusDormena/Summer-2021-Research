ea93621967b37014e5b573c340b125266aeeefa8
Marcus Dormena <mdormena@hamilton.internal.amnh.org>
2021-07-21 18:25:45 +0000

    Updating array issue: cont'd

--- findBHnew.py
+++ findBHnew.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from pynbody import filt, array
 import pandas as pd
 
+newArray=[21,3]
 s=pynbody.load('/mnt/data0/jillian/h258/stampedetesting/glenna/take2/h258.cosmo50cmb.3072gst1bwdK1BH.000048')
 h=s.halos()
 s.physical_units
@@ -23,16 +24,14 @@ def findBHhalos(s):
 BHhalos = findBHhalos(s)
 
 #To find how far BH's are from thier galaxy's center:
-#This array should be 21 rows x 3 columns!!!!
-with pynbody.analysis.halo.center(h[4], mode='hyb'):
-
+pynbody.analysis.halo.center(h[4], mode='hyb'):
     df = pd.DataFrame(data=BHhalos, columns=['Galaxy'])
     print("These are the galaxies in which each Black Hole is in:" ,h[4]['pos'][0],h[4]['pos'][1], h[4]['pos'][2])
 
     #print(h[4]['pos'][0])
     #print(h[4]['pos'][1])
     #print(h[4]['pos'][2])
-#Above, [0],[1], and [2] mean x, y, and z respectively. Not sure about h[4] though
+#Above, [0],[1], and [2] mean x, y, and z respectively.
 #position of BH:
 BHposition = BH['pos']
 print("Here are the Black Hole Positions:", BHposition)
@@ -52,9 +51,8 @@ distance = ((BHx2-BHx1)**2+(BHy2-BHy1)**2+(BHz2-BHz1)**2)**0.5
 print("The distance between the Black Hole and the Galaxy center is ", distance, "kiloparsecs.")
 
 #Table with BH ID#, Host Galaxy, and Distance from Center
-#newArray = array with ID#, galaxy, and distance. aka array of arrays
+#newArray = array with ID#, galaxy, and distance.
 # BH?, BHhalos, distance 
-#newArray = 
 #df2 = pd.DataFrame(data=newArray, columns=['Black Hole ID#','Host Galaxy','Distance (kpc)'])
 #print(df2)
 print(BH)
