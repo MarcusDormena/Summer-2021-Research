cfde7c383f7df023b3d05bccdae5e0b016f1eb81
Marcus Dormena <mdormena@hamilton.internal.amnh.org>
2021-08-01 19:32:38 +0000

    Merger #2

--- BHmerger.py
+++ BHmerger.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from pynbody import filt, array
 import pandas as pd
 
-s=pynbody.load('/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000192/h242.cosmo50PLK.3072gst5HbwK1BH.000192')
+s=pynbody.load('/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000225/h242.cosmo50PLK.3072gst5HbwK1BH.000225')
 h=s.halos()
 s.physical_units()
 print(pynbody.analysis.cosmology.age(s),"Gyrs old")
@@ -12,7 +12,7 @@ print("Redshift:",s.properties['z'])
 #function to find black holes:
 def findBH(s):
     #BHfilter = pynbody.filt.LowPass('tform', 0.0)
-    BHfilter = np.where((s.stars['iord']==75288848)|(s.stars['iord']==75289477))
+    BHfilter = np.where((s.stars['iord']==75289317)|(s.stars['iord']==75289686))
     BH = s.stars[BHfilter]
     return BH
 BH = findBH(s)
