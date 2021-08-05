3e6d64a92b9d6935c804a169503d730f9eecb576
Marcus Dormena <mdormena@hamilton.internal.amnh.org>
2021-08-05 17:54:16 +0000

    Idk what's different

--- tutorial.py
+++ tutorial.py
@@ -4,7 +4,7 @@ import pylab
 s = pynbody.load('/mnt/data0/jillian/testdata/g15784.lr.01024.gz')
 h = s.halos()
 h1 = h[1]
-print('ngas = &e, ndark = %e, nstar = %e\n'%(len(h1.gas),len(h1.dark),len(h1.star)))
+print('ngas = %e, ndark = %e, nstar = %e\n'%(len(h1.gas),len(h1.dark),len(h1.star)))
 pynbody.analysis.halo.center(h1, mode='hyb')
 print(h[1]['pos'][0])
 print(h[5]['pos'][0])
@@ -25,4 +25,22 @@ print(cen_pot)
 s['pos'] -= cen_hyb
 s.physical_units()
 pynbody.plot.image(h1.g, width = 100, cmap='Blues')
+pynbody.plot.image(s.d[pynbody.filt.Sphere('10 Mpc')], width='10 Mpc', units = 'Msol kpc^-2', cmap='Greys');
+pynbody.analysis.angmom.sideon(h1, cen=(0,0,0))
+pynbody.plot.image(h1.g, width=100, cmap='Blues');
+s.rotate_x(90)
+ps = pynbody.analysis.profile.Profile(h1.s, min = 0.01, max = 50, type = 'log')
+pylab.clf()
+pylab.plot(ps['rbins'], ps['density']);
+pylab.semilogy();
+pylab.xlabel('$R$ [kpc]');
+pylab.ylabel('$\Sigma$ [M$_\odot$/kpc$^2$]');
+pylab.figure()
+pd = pynbody.analysis.profile.Profile(h1.d,min=.01,max=50, type = 'log')
+pg = pynbody.analysis.profile.Profile(h1.g,min=.01,max=50, type = 'log')
+p = pynbody.analysis.profile.Profile(h1,min=.01,max=50, type = 'log')
+for prof, name in zip([p,pd,ps,pg],['total','dm','stars','gas']) : pylab.plot(prof['rbins'],prof['v_circ'],label=name)
+pylab.xlabel('$R$ [kpc]');
+pylab.ylabel('$v_{circ}$ [km/s]');
+pylab.legend()
 plt.show()
