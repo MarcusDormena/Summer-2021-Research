facf77fbb34791405c376189e514227294932713
Marcus Dormena <mdormena@hamilton.internal.amnh.org>
2022-01-11 21:45:27 +0000

    Added for loop for BH mergers

--- BHmerger.py
+++ BHmerger.py
@@ -7,8 +7,8 @@ import pandas as pd
 #For mergers, adjust snapshot times twice, after you see bwK1BH."blah". That's where you adjust the times
 
 #Mergers 1 and 2
-#s=pynbody.load('/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000192/h242.cosmo50PLK.3072gst5HbwK1BH.000192')
-#s=pynbody.load('/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000225/h242.cosmo50PLK.3072gst5HbwK1BH.000225')
+s=pynbody.load('/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000192/h242.cosmo50PLK.3072gst5HbwK1BH.000192')
+s=pynbody.load('/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000225/h242.cosmo50PLK.3072gst5HbwK1BH.000225')
 
 #Mergers 3 and 4
 #s=pynbody.load('/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000288/h242.cosmo50PLK.3072gst5HbwK1BH.000288')
@@ -56,8 +56,39 @@ def findBH(s):
  
 #[iord]== "blah" is where you add the BH ID #, put more massive BH first    
     BHfilter = np.where((s.stars['iord']==75288848)|(s.stars['iord']==75289477))
-    BH = s.stars[BHfilter]
-    return BH
+    BHfilter2 = np.where((s.stars['iord']==75289317)|(s.stars['iord']==75289686))
+    BHfilter3 = np.where((s.stars['iord']==75288553)|(s.stars['iord']==75288848))
+    BHfilter4 = np.where((s.stars['iord']==75288614)|(s.stars['iord']==75288740))
+    BHfilter5 = np.where((s.stars['iord']==75288565)|(s.stars['iord']==75288831))
+    BHfilter6 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75289109))
+    BHfilter7 = np.where((s.stars['iord']==75288553)|(s.stars['iord']==75288953))
+    BHfilter8 = np.where((s.stars['iord']==75288553)|(s.stars['iord']==75289347))
+    BHfilter9 = np.where((s.stars['iord']==75288614)|(s.stars['iord']==75288843))
+    BHfilter10 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75288553))
+    BHfilter11 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75288614))
+    BHfilter12 = np.where((s.stars['iord']==75288509)|(s.stars['iord']==75289317))
+
+    BH[1] = s.stars[BHfilter]
+    BH[2] = s.stars[BHfilter2]
+    BH[3] = s.stars[BHfilter3]
+    BH[4] = s.stars[BHfilter4]
+    BH[5] = s.stars[BHfilter5]
+    BH[6] = s.stars[BHfilter6]
+    BH[7] = s.stars[BHfilter7]
+    BH[8] = s.stars[BHfilter8]
+    BH[9] = s.stars[BHfilter9]
+    BH[10] = s.stars[BHfilter10]
+    BH[11] = s.stars[BHfilter11]
+    BH[12] = s.stars[BHfilter12]
+
+    #Maybe insert for loop here? For BH
+
+int i = 0
+for (i<13):
+    i = i+1
+    print BH[i]
+
+#    return BH
 BH = findBH(s)
 #print("The number of Black Holes is",len(BH))
 
