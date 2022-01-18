b4a90ee88bf34ec8f5c15a7ad5dd0ca4deb7450a
Marcus Dormena <mdormena@hamilton.internal.amnh.org>
2022-01-18 05:34:51 +0000

    added filenamelist, there is an error in here somewhere...

--- BHmerger.py
+++ BHmerger.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 from pynbody import filt, array
 import pandas as pd
 
+#filenamelist = ['/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000192/h242.cosmo50PLK.3072gst5HbwK1BH.000192','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000225/h242.cosmo50PLK.3072gst5HbwK1BH.000225','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000192/h242.cosmo50PLK.3072gst5HbwK1BH.000192','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000225/h242.cosmo50PLK.3072gst5HbwK1BH.000225','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000288/h242.cosmo50PLK.3072gst5HbwK1BH.000288','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000347/h242.cosmo50PLK.3072gst5HbwK1BH.000347','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000288/h242.cosmo50PLK.3072gst5HbwK1BH.000288','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000347/h242.cosmo50PLK.3072gst5HbwK1BH.000347','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000384/h242.cosmo50PLK.3072gst5HbwK1BH.000384','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000456/h242.cosmo50PLK.3072gst5HbwK1BH.000456','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000637/h242.cosmo50PLK.3072gst5HbwK1BH.000637','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.000672/h242.cosmo50PLK.3072gst5HbwK1BH.000672','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.001269/h242.cosmo50PLK.3072gst5HbwK1BH.001269','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.001344/h242.cosmo50PLK.3072gst5HbwK1BH.001344','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.001536/h242.cosmo50PLK.3072gst5HbwK1BH.001536','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.001632/h242.cosmo50PLK.3072gst5HbwK1BH.001632','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.002400/h242.cosmo50PLK.3072gst5HbwK1BH.002400','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.002496/h242.cosmo50PLK.3072gst5HbwK1BH.002496','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.002592/h242.cosmo50PLK.3072gst5HbwK1BH.002592','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.002688/h242.cosmo50PLK.3072gst5HbwK1BH.002688','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.002592/h242.cosmo50PLK.3072gst5HbwK1BH.002592','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.002688/h242.cosmo50PLK.3072gst5HbwK1BH.002688','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.003840/h242.cosmo50PLK.3072gst5HbwK1BH.003840','/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.003936/h242.cosmo50PLK.3072gst5HbwK1BH.003936']
+
+nfilenames = len(filenamelist)  # this will be 24 when all the files are entered.
+#nfilenames = 24  # delete this line later
+​
+BHiordlist = [75288848,75289477,75289317,75289686,75288553,75288848,75288614,75288740,75288565,75288831,75288505,75289109,75288553,75288953,75288553,75289347,75288614,75288843,75288505,75288553,75288505,75288614,75288505,75289317]
+
 #For mergers, adjust snapshot times twice, after you see bwK1BH."blah". That's where you adjust the times
 
 #Mergers 1 and 2
@@ -46,47 +53,62 @@ import pandas as pd
 #s=pynbody.load('/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.003840/h242.cosmo50PLK.3072gst5HbwK1BH.003840')
 #s=pynbody.load('/mnt/data0/jillian/h242/h242.cosmo50PLK.3072gst5HbwK1BH.003936/h242.cosmo50PLK.3072gst5HbwK1BH.003936')
 
-h=s.halos()
-s.physical_units()
-print(pynbody.analysis.cosmology.age(s),"Gyrs old")
-print("Redshift:",s.properties['z'])
-#function to find black holes:
-def findBH(s):
-    #BHfilter = pynbody.filt.LowPass('tform', 0.0)
- 
-#[iord]== "blah" is where you add the BH ID #, put more massive BH first    
-    BHfilter = np.where((s.stars['iord']==75288848)|(s.stars['iord']==75289477))
-    BHfilter2 = np.where((s.stars['iord']==75289317)|(s.stars['iord']==75289686))
-    BHfilter3 = np.where((s.stars['iord']==75288553)|(s.stars['iord']==75288848))
-    BHfilter4 = np.where((s.stars['iord']==75288614)|(s.stars['iord']==75288740))
-    BHfilter5 = np.where((s.stars['iord']==75288565)|(s.stars['iord']==75288831))
-    BHfilter6 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75289109))
-    BHfilter7 = np.where((s.stars['iord']==75288553)|(s.stars['iord']==75288953))
-    BHfilter8 = np.where((s.stars['iord']==75288553)|(s.stars['iord']==75289347))
-    BHfilter9 = np.where((s.stars['iord']==75288614)|(s.stars['iord']==75288843))
-    BHfilter10 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75288553))
-    BHfilter11 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75288614))
-    BHfilter12 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75289317))
+#h=s.halos()
+#s.physical_units()
+#print(pynbody.analysis.cosmology.age(s),"Gyrs old")
+#print("Redshift:",s.properties['z'])
 
-    BH[1] = s.stars[BHfilter]
-    BH[2] = s.stars[BHfilter2]
-    BH[3] = s.stars[BHfilter3]
-    BH[4] = s.stars[BHfilter4]
-    BH[5] = s.stars[BHfilter5]
-    BH[6] = s.stars[BHfilter6]
-    BH[7] = s.stars[BHfilter7]
-    BH[8] = s.stars[BHfilter8]
-    BH[9] = s.stars[BHfilter9]
-    BH[10] = s.stars[BHfilter10]
-    BH[11] = s.stars[BHfilter11]
-    BH[12] = s.stars[BHfilter12]
+#function to find black holes:
+#def findBH(s):
+    #BHfilter = pynbody.filt.LowPass('tform', 0.0)
+
+k=0
+for j in range(nfilenames):
+    print("j = ",j)
+    s = pynbody.load(filenamelist[j])
+    h=s.halos()
+    s.physical_units()
+    print(pynbody.analysis.cosmology.age(s),"Gyrs old")
+    print("Redshift:",s.properties['z'])
+    
+#[iord]== "blah" is where you add the BH ID #, put more massive BH first    
+
+    print("k = ",k)
+    BHfilter = np.where((s.stars['iord']==BHiordlist[k])|(s.stars['iord']==BHiordlist[k+1]))
+    BH = s.stars[BHfilter]
+
+#    BHfilter = np.where((s.stars['iord']==75288848)|(s.stars['iord']==75289477))
+#    BHfilter2 = np.where((s.stars['iord']==75289317)|(s.stars['iord']==75289686))
+#    BHfilter3 = np.where((s.stars['iord']==75288553)|(s.stars['iord']==75288848))
+#    BHfilter4 = np.where((s.stars['iord']==75288614)|(s.stars['iord']==75288740))
+#    BHfilter5 = np.where((s.stars['iord']==75288565)|(s.stars['iord']==75288831))
+#    BHfilter6 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75289109))
+#    BHfilter7 = np.where((s.stars['iord']==75288553)|(s.stars['iord']==75288953))
+#    BHfilter8 = np.where((s.stars['iord']==75288553)|(s.stars['iord']==75289347))
+#    BHfilter9 = np.where((s.stars['iord']==75288614)|(s.stars['iord']==75288843))
+#    BHfilter10 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75288553))
+#    BHfilter11 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75288614))
+#    BHfilter12 = np.where((s.stars['iord']==75288505)|(s.stars['iord']==75289317))
+
+#    BH[1] = s.stars[BHfilter]
+#    BH[2] = s.stars[BHfilter2]
+#    BH[3] = s.stars[BHfilter3]
+#    BH[4] = s.stars[BHfilter4]
+#    BH[5] = s.stars[BHfilter5]
+#    BH[6] = s.stars[BHfilter6]
+#    BH[7] = s.stars[BHfilter7]
+#    BH[8] = s.stars[BHfilter8]
+#    BH[9] = s.stars[BHfilter9]
+#    BH[10] = s.stars[BHfilter10]
+#    BH[11] = s.stars[BHfilter11]
+#    BH[12] = s.stars[BHfilter12]
 
     #Maybe insert for loop here? For BH
 
-int i = 0
-for (i<13):
-    i = i+1
-    print BH[i]
+#int i = 0
+#for (i<13):
+#    i = i+1
+#    print BH[i]
 
 #    return BH
 BH = findBH(s)
